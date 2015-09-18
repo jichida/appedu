2c575d4ca25a49aa300eda10c4b81e19ba26060b
wangxiaoqing <downup2u@126.com>
2015-09-18 15:24:52 +0800

    childlist

---
+++ appedu/client/app/child/childlist.js
@@ -0,0 +1,14 @@
+Template.childlist.events({
+    "click #btneditchild": function () {
+      console.log("click btn btneditchild");
+      event.preventDefault();
+    },
+    "click #btnexitclass": function () {
+      console.log("click btn btnexitclass");
+      event.preventDefault();
+    },
+    "click #btndeletechild": function () {
+      console.log("click btn btndeletechild");
+      event.preventDefault();
+    },
+  });
